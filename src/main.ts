aa585194f690a5f8059c1832718445e3c77f56fd
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-06-16 19:22:27 -0300

    Remoção da documentação do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
@@ -10,16 +9,6 @@ async function bootstrap() {
 	app.enableCors()
 
 	const appConfig = app.get(ConfigService)
-
-	const swaggerConfig = new DocumentBuilder()
-		.addTag('DevShop API')
-		.setTitle('DevShop API')
-		.setDescription('Documentação da API DevShop')
-		.setVersion('1.0')
-		.build()
-
-	const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
-	SwaggerModule.setup('docs', app, swaggerDocument)
 
 	const PORT = appConfig.get('PORT', 3003)
 	await app.listen(PORT)
