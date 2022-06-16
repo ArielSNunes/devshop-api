c75547118b468467b8c6fbac31ece07bf99e1de5
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-06-16 19:15:41 -0300

    Ajustes para criação de categorias via graphql

--- src/categories/categories.resolver.ts
+++ src/categories/categories.resolver.ts
@@ -1,12 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { CategoriesService } from './categories.service'
+import { CategoryCreateInput } from './dto/category-create.input'
 import { CategoryPublic } from './dto/category.gql'
 
 @Resolver()
 export class CategoriesResolver {
 	constructor(private readonly categoryService: CategoriesService) {}
+
 	@Query(returns => [CategoryPublic], { name: 'getAllCategories' })
 	async getAllCategories(): Promise<CategoryPublic[]> {
 		return await this.categoryService.findAll()
+	}
+
+	@Mutation(returns => CategoryPublic, { name: 'createCategory' })
+	async createCategory(
+		@Args('input') input: CategoryCreateInput,
+	): Promise<CategoryPublic> {
+		return await this.categoryService.create(input)
 	}
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common'
 import { Category } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { CategoryCreateInput } from './dto/category-create.input'
 
 @Injectable()
 export class CategoriesService {
 	constructor(private readonly prismaService: PrismaService) {}
+
 	async findAll(): Promise<Category[]> {
 		return await this.prismaService.category.findMany()
+	}
+
+	async create(input: CategoryCreateInput): Promise<Category> {
+		return await this.prismaService.category.create({
+			data: input,
+		})
 	}
 }
