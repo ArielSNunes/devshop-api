b001c241b46e494d06411e510dca54edca8afee8
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-06-16 17:51:28 -0300

    Inclusão de configs para documentação do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+	app.enableCors()
+
+	const appConfig = app.get(ConfigService)
+
+	const swaggerConfig = new DocumentBuilder()
+		.addTag('DevShop API')
+		.setTitle('DevShop API')
+		.setDescription('Documentação da API DevShop')
+		.setVersion('1.0')
+		.build()
+
+	const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+	SwaggerModule.setup('docs', app, swaggerDocument)
+
+	const PORT = appConfig.get('PORT', 3003)
+	await app.listen(PORT)
 }
 bootstrap()
